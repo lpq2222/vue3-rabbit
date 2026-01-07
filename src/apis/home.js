8ef7d1debaaf1101f77bc63b25654f4d8ba3e7ec
lyq4444 <peiqiliu2222@gmail.com>
2026-01-07 21:25:31 +0800

    home-图片懒加载指令实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,7 +5,12 @@ export function getBannerAPI() {
         url:'/home/banner'
     })
 }
-
+//获取新鲜好物
 export function getNewApi() {
   return httpInstance({ url: '/home/new' });
+}
+
+//获取人气推荐
+export function getHotApi() {
+  return httpInstance({ url: '/home/hot' });
 }
