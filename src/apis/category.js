40d44c1a5a49084bfd50d6566eeebe14b1d348e3
lyq4444 <peiqiliu2222@gmail.com>
2026-01-09 18:10:10 +0800

    一级分类-面包屑导航渲染

---
+++ src/apis/category.js
@@ -0,0 +1,10 @@
+﻿import request from '@/utils/http'
+
+export function getCategoryAPI(id) {
+    return request({
+        url: '/category',
+        params: {
+            id
+        }
+    })
+}
